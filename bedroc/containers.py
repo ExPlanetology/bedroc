c22ca20b9631b2b532f9353b31a2559900c23157
Dan J Bower <djbower@users.noreply.github.com>
2025-11-19 14:37:01 +0100

    feat: use suffices for features and their standard deviations

--- bedroc/containers.py
+++ bedroc/containers.py
@@ -19,7 +19,7 @@
 import logging
 from collections.abc import Iterable
 from pathlib import Path
-from typing import Any, Optional
+from typing import Any, Optional, cast
 
 import numpy as np
 import numpy.typing as npt
@@ -41,10 +41,11 @@ class DataContainer:
     Args:
         dataframe: A dataframe with columns of feature values and their standard deviations
         name: Data container name. Defaults to ``data``.
-        feature_prefix: Prefix for feature value columns. Defaults to ``Feat``.
-        feature_std_prefix: Prefix of feature standard deviation columns. Defaults to ``Unc``.
+        feature_suffix: Suffix of feature value columns. Defaults to ``_feature``.
+        feature_std_suffix: Suffix of feature standard deviation columns. Defaults to
+            ``_uncertainty``.
         select_features: An optional iterable (tuple or list) of bare feature names (without
-            prefix) to select. Defaults to ``None`` to select all features.
+            ``feature_suffix``) to select. Defaults to ``None`` to select all features.
         select_data: An optional iterable (tuple or list) of data to select. Defaults to ``None``
             to select all data.
         data_column: Name of the data column used by ``select_data``. Defaults to ``ID``.
@@ -55,38 +56,41 @@ class DataContainer:
         dataframe: pd.DataFrame,
         *,
         name: str = "data",
-        feature_prefix: str = "",
-        feature_std_prefix: str = "Unc",
+        feature_suffix: str = "_feature",
+        feature_std_suffix: str = "_uncertainty",
         select_features: Optional[Iterable[str]] = None,
         select_data: Optional[Iterable[Any]] = None,
         data_column: str = "ID",
     ):
         if select_features is not None:
-            suffix_tuple: tuple[str, ...] = tuple(select_features)
+            feature: tuple[str, ...] = tuple(select_features)
 
             cols = dataframe.columns
 
-            # Rule 1: keep columns that do NOT start with prefix
-            keep_non_prefix = ~cols.str.startswith((feature_prefix, feature_std_prefix))
+            # Rule 1: keep columns that are not features or feature standard deviations
+            keep_non_suffix = ~cols.str.endswith((feature_suffix, feature_std_suffix))
 
-            # Rule 2: keep columns that DO start with prefix AND match suffix
-                (feature_prefix, feature_std_prefix)
-            ) & cols.str.endswith(suffix_tuple)
+            # Rule 2: keep columns that match a feature name
+            keep_feature_and_suffix = cols.str.startswith(feature) & cols.str.endswith(
+                (feature_suffix, feature_std_suffix)
+            )
 
             # Select features based on the column suffix
-            mask = keep_non_prefix | keep_prefix_and_suffix
+            mask = keep_non_suffix | keep_feature_and_suffix
 
             dataframe = dataframe.loc[:, mask].copy()  # Avoid aliasing
 
         if select_data is not None:
             data_tuple: tuple[str, ...] = tuple(select_data)
-            dataframe = dataframe[dataframe[data_column].isin(data_tuple)].copy()  # Avoid aliasing
+            dataframe = cast(
+                pd.DataFrame, dataframe[dataframe[data_column].isin(data_tuple)].copy()
+            )  # Avoid aliasing
 
         # Always store an independent copy of the raw data internally
         self.df_raw: pd.DataFrame = dataframe.copy()
 
-        self.feature_prefix: str = feature_prefix
-        self.feature_std_prefix: str = feature_std_prefix
+        self.feature_suffix: str = feature_suffix
+        self.feature_std_suffix: str = feature_std_suffix
 
         # Scaling parameters computed from raw data
         self.scaling_means: pd.Series = self._compute_scaling_means()
@@ -117,17 +121,17 @@ class DataContainer:
     @property
     def feature_columns(self) -> pd.Index:
         """Index of feature columns"""
-        return self.df_raw.columns[self.df_raw.columns.str.startswith(self.feature_prefix)]
+        return self.df_raw.columns[self.df_raw.columns.str.endswith(self.feature_suffix)]
 
     @property
     def feature_std_columns(self) -> pd.Index:
         """Index of feature uncertainty columns"""
-        return self.df_raw.columns[self.df_raw.columns.str.startswith(self.feature_std_prefix)]
+        return self.df_raw.columns[self.df_raw.columns.str.endswith(self.feature_std_suffix)]
 
     @property
     def feature_names(self) -> pd.Index:
         """Index of feature names with the prefix removed"""
-        return self.df_raw[self.feature_columns].columns.str.removeprefix(self.feature_prefix)
+        return self.df_raw[self.feature_columns].columns.str.removesuffix(self.feature_suffix)
 
     @property
     def n_data(self) -> int:
@@ -160,7 +164,7 @@ class DataContainer:
         # Rename the indices for correct broadcasting
         scaling_stds_unc: pd.Series = self.scaling_stds.copy()
         scaling_stds_unc.index = self.scaling_stds.index.str.replace(
-            self.feature_prefix, self.feature_std_prefix
+            self.feature_suffix, self.feature_std_suffix
         )
         # Standardize feature standard deviations
         df[self.feature_std_columns] = df[self.feature_std_columns] / scaling_stds_unc
