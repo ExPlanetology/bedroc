28bce8e4fca263e3782217e7d436c6ae6d4b0706
Dan J Bower <djbower@users.noreply.github.com>
2026-08-10 13:26:11 +0200

    Create __init__.py

---
+++ bedroc/difference/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2025 Dan J. Bower <dbower@eaps.ethz.ch>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Bayesian hierarchical models for quantifying group differences and classification."""
