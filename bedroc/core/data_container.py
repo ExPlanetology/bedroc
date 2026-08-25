42c315007e3c68ebbb8048671521161c06dbf740
Dan J Bower <djbower@users.noreply.github.com>
2026-08-25 11:42:10 +0200

    chore: comment

--- bedroc/core/data_container.py
+++ bedroc/core/data_container.py
@@ -330,10 +330,11 @@ class DataContainer:
         """Plots a heatmap of the Pearson correlation coefficient.
 
         Returns:
-            Figure axes.
+            Figure axes
         """
+        # Compute pairwise correlation of columns, excluding NA/null values
+        # equivalent to np.correcoef(self.values, rowvar=False)
         corr_matrix: pd.DataFrame = self.values.corr()
-
         ax: Axes = sns.heatmap(corr_matrix, cmap="magma", annot=True, fmt=".2f", vmin=-1, vmax=1)
         ax.set_title("Pearson correlation coefficient")
 
